54654fd760b3dc165ca5c9f9221569c1396072a6
NyuB <brice.decaestecker@gmx.fr>
2023-12-03 21:18:18 +0100

    Day 03 cleaned

--- src/aoc_2023_03.zig
+++ src/aoc_2023_03.zig
@@ -15,34 +15,6 @@ pub fn solve_part_one(allocator: std.mem.Allocator, lines: std.ArrayList(String)
     return res;
 }
 
-fn scanParts(allocator: std.mem.Allocator, grid: Grid) !std.ArrayList(i32) {
-    var res = std.ArrayList(i32).init(allocator);
-    for (0..grid.height) |row| {
-        try scanPartsInRow(grid, row, &res);
-    }
-    return res;
-}
-
-fn scanPartsInRow(grid: Grid, row: usize, into: *std.ArrayList(i32)) !void {
-    var digitChunk: ?Grid.Chunk = null;
-    for (0..grid.width) |c| {
-        switch (grid.at(row, c)) {
-            .Digit => |_| {
-                if (digitChunk) |*chunk| {
-                    chunk.extend();
-                } else {
-                    digitChunk = Grid.Chunk{ .row = row, .col_start = c, .col_end = c };
-                }
-            },
-            else => {
-                try appendChunkIfPresentAndSymbol(grid, digitChunk, into);
-                digitChunk = null;
-            },
-        }
-    }
-    try appendChunkIfPresentAndSymbol(grid, digitChunk, into);
-}
-
 pub fn solve_part_two(allocator: std.mem.Allocator, lines: std.ArrayList(String)) i32 {
     var grid = Grid.parse(allocator, lines) catch unreachable;
     defer grid.deinit();
@@ -51,16 +23,72 @@ pub fn solve_part_two(allocator: std.mem.Allocator, lines: std.ArrayList(String)
     return sumGears(gears);
 }
 
-const ScanType = struct { map: *GearMap, grid: Grid, chunk: Grid.Chunk };
-fn scanGears(allocator: std.mem.Allocator, grid: Grid) !GearMap {
-    var res = GearMap.init(allocator);
+const PartScan = struct {
+    list: *std.ArrayList(i32),
+    grid: Grid,
+    fn scanChunk(scan: *PartScan, c: Grid.Chunk) !void {
+        if (try atLeastOneSymbolAroundChunk(scan.grid, c)) {
+            const n = chunkAsNumber(scan.grid, c);
+            try scan.list.append(n);
+        }
+    }
+};
+
+fn scanParts(allocator: std.mem.Allocator, grid: Grid) !std.ArrayList(i32) {
+    var res = std.ArrayList(i32).init(allocator);
+    var scan = PartScan{ .list = &res, .grid = grid };
     for (0..grid.height) |row| {
-        try scanGearsInRow(grid, row, &res);
+        try forEachChunkInRow(grid, row, &scan, PartScan.scanChunk);
     }
     return res;
 }
 
-fn scanGearsInRow(grid: Grid, row: usize, map: *GearMap) !void {
+const GearScan = struct {
+    map: *GearMap,
+    grid: Grid,
+    chunk: ?Grid.Chunk,
+
+    fn scanChunk(scan: *GearScan, c: Grid.Chunk) !void {
+        scan.chunk = c;
+        try scan.grid.forEachTileAroundChunk(*GearScan, GearScan.scanGearItem, scan, c);
+    }
+
+    fn scanGearItem(scanState: *GearScan, rowCol: RowCol, tile: Tile) !void {
+        if (scanState.chunk) |chunk| {
+            switch (tile) {
+                .Symbol => |s| {
+                    if (s == '*') {
+                        const findOpt = scanState.map.get(rowCol);
+                        if (findOpt) |gear| {
+                            switch (gear) {
+                                .OneChunk => |i| {
+                                    try scanState.map.put(rowCol, GearCandidate{ .TwoChunks = i * chunkAsNumber(scanState.grid, chunk) });
+                                },
+                                else => {
+                                    try scanState.map.put(rowCol, GearCandidate{ .MoreThanTwoChunks = {} });
+                                },
+                            }
+                        } else {
+                            try scanState.map.put(rowCol, GearCandidate{ .OneChunk = chunkAsNumber(scanState.grid, chunk) });
+                        }
+                    }
+                },
+                else => {},
+            }
+        }
+    }
+};
+
+fn scanGears(allocator: std.mem.Allocator, grid: Grid) !GearMap {
+    var res = GearMap.init(allocator);
+    var scan = GearScan{ .map = &res, .grid = grid, .chunk = null };
+    for (0..grid.height) |row| {
+        try forEachChunkInRow(grid, row, &scan, GearScan.scanChunk);
+    }
+    return res;
+}
+
+fn forEachChunkInRow(grid: Grid, row: usize, state: anytype, Do: *const fn (@TypeOf(state), Grid.Chunk) anyerror!void) anyerror!void {
     var digitChunk: ?Grid.Chunk = null;
     for (0..grid.width) |c| {
         switch (grid.at(row, c)) {
@@ -74,41 +102,15 @@ fn scanGearsInRow(grid: Grid, row: usize, map: *GearMap) !void {
             },
             else => {
                 if (digitChunk) |chunk| {
-                    var acc = ScanType{ .map = map, .grid = grid, .chunk = chunk };
-                    _ = grid.forEachTileAroundChunk(*ScanType, scanGearItem, &acc, chunk);
+                    try Do(state, chunk);
                 }
                 digitChunk = null;
             },
         }
     }
     if (digitChunk) |chunk| {
-        var acc = ScanType{ .map = map, .grid = grid, .chunk = chunk };
-        _ = grid.forEachTileAroundChunk(*ScanType, scanGearItem, &acc, chunk);
+        try Do(state, chunk);
     }
-}
-
-fn scanGearItem(acc: *ScanType, rowCol: RowCol, tile: Tile) *ScanType {
-    switch (tile) {
-        .Symbol => |s| {
-            if (s == '*') {
-                const findOpt = acc.map.get(rowCol);
-                if (findOpt) |gear| {
-                    switch (gear) {
-                        .OneChunk => |i| {
-                            acc.map.put(rowCol, GearCandidate{ .TwoChunks = i * chunkAsNumber(acc.grid, acc.chunk) }) catch unreachable;
-                        },
-                        else => {
-                            acc.map.put(rowCol, GearCandidate{ .MoreThanTwoChunks = {} }) catch unreachable;
-                        },
-                    }
-                } else {
-                    acc.map.put(rowCol, GearCandidate{ .OneChunk = chunkAsNumber(acc.grid, acc.chunk) }) catch unreachable;
-                }
-            }
-        },
-        else => {},
-    }
-    return acc;
 }
 
 fn sumGears(map: GearMap) i32 {
@@ -123,14 +125,6 @@ fn sumGears(map: GearMap) i32 {
         }
     }
     return res;
-}
-
-fn appendChunkIfPresentAndSymbol(grid: Grid, chunk: ?Grid.Chunk, into: *std.ArrayList(i32)) !void {
-    if (chunk) |c| {
-        if (atLeastOneSymbolAroundChunk(grid, c)) {
-            try into.append(chunkAsNumber(grid, c));
-        }
-    }
 }
 
 const RowCol = struct { row: usize, col: usize };
@@ -197,38 +191,37 @@ const Grid = struct {
         return grid.tiles[row][column];
     }
 
-    pub fn forEachTileAroundChunk(grid: Grid, comptime Result: type, comptime Do: *const fn (Result, RowCol, Tile) Result, init: Result, chunk: Chunk) Result {
+    pub fn forEachTileAroundChunk(grid: Grid, comptime State: type, comptime Do: *const fn (State, RowCol, Tile) anyerror!void, state: State, chunk: Chunk) anyerror!void {
         const startCol = if (chunk.col_start == 0) 0 else chunk.col_start - 1;
         const endCol = if (chunk.col_end == grid.width - 1) grid.width - 1 else chunk.col_end + 1;
-        var res = init;
         if (chunk.row > 0) {
             const topRow = chunk.row - 1;
             for (startCol..endCol + 1) |c| {
-                res = Do(res, RowCol{ .row = topRow, .col = c }, grid.at(topRow, c));
+                try Do(state, RowCol{ .row = topRow, .col = c }, grid.at(topRow, c));
             }
         }
 
         if (chunk.row < grid.height - 1) {
             const botRow = chunk.row + 1;
             for (startCol..endCol + 1) |c| {
-                res = Do(res, RowCol{ .row = botRow, .col = c }, grid.at(botRow, c));
+                try Do(state, RowCol{ .row = botRow, .col = c }, grid.at(botRow, c));
             }
         }
 
         if (chunk.col_start > 0) {
-            res = Do(res, RowCol{ .row = chunk.row, .col = chunk.col_start - 1 }, grid.at(chunk.row, chunk.col_start - 1));
+            try Do(state, RowCol{ .row = chunk.row, .col = chunk.col_start - 1 }, grid.at(chunk.row, chunk.col_start - 1));
         }
 
         if (chunk.col_end < grid.width - 1) {
-            res = Do(res, RowCol{ .row = chunk.row, .col = chunk.col_end + 1 }, grid.at(chunk.row, chunk.col_end + 1));
+            try Do(state, RowCol{ .row = chunk.row, .col = chunk.col_end + 1 }, grid.at(chunk.row, chunk.col_end + 1));
         }
-        return res;
     }
 
     const Chunk = struct {
         row: usize,
         col_start: usize,
         col_end: usize,
+
         pub fn width(c: Chunk) usize {
             return c.col_end - c.col_start + 1;
         }
@@ -245,16 +238,15 @@ fn isSymbol(tile: Tile) bool {
     };
 }
 
-fn setTrueIfSymbol(acc: *bool, _: RowCol, t: Tile) *bool {
+fn setTrueIfSymbol(flag: *bool, _: RowCol, t: Tile) !void {
     if (isSymbol(t)) {
-        acc.* = true;
+        flag.* = true;
     }
-    return acc;
 }
 
-fn atLeastOneSymbolAroundChunk(grid: Grid, chunk: Grid.Chunk) bool {
+fn atLeastOneSymbolAroundChunk(grid: Grid, chunk: Grid.Chunk) !bool {
     var res = false;
-    _ = grid.forEachTileAroundChunk(*bool, setTrueIfSymbol, &res, chunk);
+    try grid.forEachTileAroundChunk(*bool, setTrueIfSymbol, &res, chunk);
     return res;
 }
 
@@ -297,7 +289,8 @@ test "Example Part One" {
     try lines.append(".664.598..");
 
     const expected: i32 = 4361;
-    try std.testing.expectEqual(expected, solve_part_one(std.testing.allocator, lines));
+    const actual = solve_part_one(std.testing.allocator, lines);
+    try std.testing.expectEqual(expected, actual);
 }
 
 test "Example Part Two" {
@@ -351,10 +344,10 @@ test "Grid.anyAroundChunk" {
     const chunkMid = Grid.Chunk{ .row = 3, .col_start = 2, .col_end = 2 };
     const chunkBotLeft = Grid.Chunk{ .row = 5, .col_start = 0, .col_end = 2 };
     const chunkBotRight = Grid.Chunk{ .row = 5, .col_start = 5, .col_end = 5 };
-    try std.testing.expectEqual(true, atLeastOneSymbolAroundChunk(grid, chunkTop));
-    try std.testing.expectEqual(true, atLeastOneSymbolAroundChunk(grid, chunkMid));
-    try std.testing.expectEqual(false, atLeastOneSymbolAroundChunk(grid, chunkBotLeft));
-    try std.testing.expectEqual(true, atLeastOneSymbolAroundChunk(grid, chunkBotRight));
+    try std.testing.expectEqual(true, try atLeastOneSymbolAroundChunk(grid, chunkTop));
+    try std.testing.expectEqual(true, try atLeastOneSymbolAroundChunk(grid, chunkMid));
+    try std.testing.expectEqual(false, try atLeastOneSymbolAroundChunk(grid, chunkBotLeft));
+    try std.testing.expectEqual(true, try atLeastOneSymbolAroundChunk(grid, chunkBotRight));
 }
 
 test "chunkAsNumber" {
