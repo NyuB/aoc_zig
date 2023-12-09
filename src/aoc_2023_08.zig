b6c944d639e287acf1a83c95a829955d9daa7097
NyuB <brice.decaestecker@gmx.fr>
2023-12-09 17:20:57 +0100

    Day 08 solved, relies on 'untold' properties of input

--- src/aoc_2023_08.zig
+++ src/aoc_2023_08.zig
@@ -3,30 +3,47 @@ const expect = std.testing.expect;
 const lib = @import("tests_lib.zig");
 const String = lib.String;
 
-fn solve_part_one(allocator: std.mem.Allocator, lines: std.ArrayList(String)) u32 {
+const uint = u64;
+
+fn solve_part_one(allocator: std.mem.Allocator, lines: std.ArrayList(String)) uint {
     var instructions = parseDirections(allocator, lines.items[0]) catch unreachable;
     defer instructions.deinit();
-    var btree = BTree.parse(allocator, lines.items[2..]) catch unreachable;
+    var btree = Tree.parse(allocator, lines.items[2..]) catch unreachable;
     defer btree.deinit();
     return btree.distanceFollowingInstructions(instructions.items, "AAA", isZZZ) orelse unreachable;
 }
 
-fn solve_part_two(allocator: std.mem.Allocator, lines: std.ArrayList(String)) u32 {
+/// Assume A => Z distances are perfectly cyclic, i.e. if dist(iA=>iZ) == n then dist(iZ=>iZ) == n
+fn solve_part_two(allocator: std.mem.Allocator, lines: std.ArrayList(String)) uint {
     var instructions = parseDirections(allocator, lines.items[0]) catch unreachable;
     defer instructions.deinit();
-    var btree = BTree.parse(allocator, lines.items[2..]) catch unreachable;
+    var btree = Tree.parse(allocator, lines.items[2..]) catch unreachable;
     defer btree.deinit();
     var origins = std.ArrayList(String).init(allocator);
     defer origins.deinit();
-    var res: u32 = 0;
-    _ = res;
+    var res: ?uint = null;
     var starts = btree.map.keyIterator();
     while (starts.next()) |start| {
         if (endsWithA(start.*)) {
-            origins.append(start.*) catch unreachable;
+            const dist = btree.distanceFollowingInstructions(instructions.items, start.*, endsWithZ) orelse unreachable;
+            if (res) |r| {
+                res = lowestCommonFactor(r, dist);
+            } else {
+                res = dist;
+            }
         }
     }
-    return btree.parallelDistanceFollowingInstructions(allocator, instructions.items, origins.items, endsWithZ) orelse unreachable;
+    return res orelse unreachable;
+}
+
+fn lowestCommonFactor(a: uint, b: uint) uint {
+    const max = @max(a, b);
+    const min = @min(a, b);
+    var res = max;
+    while (res % min != 0) {
+        res += max;
+    }
+    return res;
 }
 
 const Direction = enum { Left, Right };
@@ -54,14 +71,14 @@ fn isZZZ(s: String) bool {
     return std.mem.eql(u8, s, "ZZZ");
 }
 
-const BTree = struct {
-    map: std.StringHashMap(BTreeNode),
-    fn deinit(self: *BTree) void {
+const Tree = struct {
+    map: std.StringHashMap(TreeNode),
+    fn deinit(self: *Tree) void {
         self.map.deinit();
     }
 
-    fn distanceFollowingInstructions(self: BTree, instructions: []Direction, origin: String, Predicate: *const fn (String) bool) ?u32 {
-        var res: u32 = 0;
+    fn distanceFollowingInstructions(self: Tree, instructions: []Direction, origin: String, Predicate: *const fn (String) bool) ?uint {
+        var res: uint = 0;
         var current = origin;
         var cycle = InstructionCycle.init(instructions);
         while (cycle.next()) |direction| {
@@ -78,36 +95,10 @@ const BTree = struct {
         return null; // We should have return from inside the loop or there is a problem with the instructions
     }
 
-    fn parallelDistanceFollowingInstructions(self: BTree, allocator: std.mem.Allocator, instructions: []Direction, origins: []String, Predicate: *const fn (String) bool) ?u32 {
-        var res: u32 = 0;
-        var currents = allocator.alloc(String, origins.len) catch return null;
-        defer allocator.free(currents);
-        std.mem.copy(String, currents, origins);
-        var cycle = InstructionCycle.init(instructions);
-        var allAligned = false;
-        while (!allAligned) {
-            allAligned = true;
-            var nextDirection = cycle.next() orelse return null;
-            for (currents, 0..) |current, index| {
-                allAligned = allAligned and Predicate(current);
-                const node = self.map.get(current) orelse return null;
-                currents[index] = switch (nextDirection) {
-                    .Left => node.left,
-                    .Right => node.right,
-                };
-            }
-
-            if (!allAligned) {
-                res += 1;
-            }
-        }
-        return res;
-    }
-
     const ParseError = error{ AllocationError, InvalidLineFormat };
 
-    fn parse(allocator: std.mem.Allocator, lines: []String) ParseError!BTree {
-        var map = std.StringHashMap(BTreeNode).init(allocator);
+    fn parse(allocator: std.mem.Allocator, lines: []String) ParseError!Tree {
+        var map = std.StringHashMap(TreeNode).init(allocator);
         errdefer map.deinit();
         for (lines) |l| {
             const origin_node = lib.split_n_str(2, l, " = ");
@@ -116,12 +107,12 @@ const BTree = struct {
             const left = (leftRightPar[0] orelse return ParseError.InvalidLineFormat)[1..];
             const rightPar = leftRightPar[1] orelse return ParseError.InvalidLineFormat;
             const right = rightPar[0 .. rightPar.len - 1];
-            map.put(origin, BTreeNode{ .left = left, .right = right }) catch return ParseError.AllocationError;
+            map.put(origin, TreeNode{ .left = left, .right = right }) catch return ParseError.AllocationError;
         }
-        return BTree{ .map = map };
+        return Tree{ .map = map };
     }
 };
-const BTreeNode = struct {
+const TreeNode = struct {
     left: String,
     right: String,
 };
@@ -148,13 +139,13 @@ const InstructionCycle = struct {
 // Tests
 
 test "Golden Test Part One" {
-    const res = try lib.for_lines_allocating(u32, std.testing.allocator, "problems/08.txt", solve_part_one);
-    try std.testing.expectEqual(@as(u32, 20093), res);
+    const res = try lib.for_lines_allocating(uint, std.testing.allocator, "problems/08.txt", solve_part_one);
+    try std.testing.expectEqual(@as(uint, 20093), res);
 }
-// test "Golden Test Part Two" {
-//     const res = try lib.for_lines_allocating(u32, std.testing.allocator, "problems/08.txt", solve_part_two);
-//     try std.testing.expectEqual(@as(u32, 0), res);
-// }
+test "Golden Test Part Two" {
+    const res = try lib.for_lines_allocating(uint, std.testing.allocator, "problems/08.txt", solve_part_two);
+    try std.testing.expectEqual(@as(uint, 22103062509257), res);
+}
 
 test "Example Part One" {
     var lines = std.ArrayList(String).init(std.testing.allocator);
@@ -169,7 +160,7 @@ test "Example Part One" {
     try lines.append("GGG = (GGG, GGG)");
     try lines.append("ZZZ = (ZZZ, ZZZ)");
     const res = solve_part_one(std.testing.allocator, lines);
-    try std.testing.expectEqual(@as(u32, 2), res);
+    try std.testing.expectEqual(@as(uint, 2), res);
 }
 
 test "Example Part One cyclic required" {
@@ -181,7 +172,7 @@ test "Example Part One cyclic required" {
     try lines.append("BBB = (AAA, ZZZ)");
     try lines.append("ZZZ = (ZZZ, ZZZ)");
     const res = solve_part_one(std.testing.allocator, lines);
-    try std.testing.expectEqual(@as(u32, 6), res);
+    try std.testing.expectEqual(@as(uint, 6), res);
 }
 
 test "Example Part Two" {
@@ -198,11 +189,11 @@ test "Example Part Two" {
     try lines.append("22Z = (22B, 22B)");
     try lines.append("XXX = (XXX, XXX)");
     const res = solve_part_two(std.testing.allocator, lines);
-    try std.testing.expectEqual(@as(u32, 6), res);
+    try std.testing.expectEqual(@as(uint, 6), res);
 }
 
 test "Cyclic instructions" {
-    var iterationCount: u32 = 0;
+    var iterationCount: uint = 0;
     const instructions = [_]Direction{ Direction.Left, Direction.Left, Direction.Left };
     var it = InstructionCycle.init(&instructions);
     while (it.next()) |_| {
@@ -210,4 +201,10 @@ test "Cyclic instructions" {
         if (iterationCount == 42) break;
     }
     try expect(iterationCount == 42);
+}
+
+test "lowestCommonFactor" {
+    try expect(lowestCommonFactor(2, 3) == 6);
+    try expect(lowestCommonFactor(4, 2) == 4);
+    try expect(lowestCommonFactor(7, 3) == 21);
 }
